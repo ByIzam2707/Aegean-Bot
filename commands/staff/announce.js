cffbd39d4b73d3c48826392f204e98a3f003f581
ByIzam2707 <69021686+ByIzam2707@users.noreply.github.com>
2020-08-14 20:04:50 +0100

    Fixed announce.js

--- commands/staff/announce.js
+++ commands/staff/announce.js
@@ -7,7 +7,7 @@ description: 'Makes an announcement with the message provided.'
 run: async(bot, message, args)=>{
 let rChannel = message.guild.channels.cache.get(args[0])
 if(!rChannel)return = message.channel.send(`Hey, tell me the channel.`)
-let MSG = message.content.split(`${bot.prefix}shout ${rChannel}`).join(" ")
+let MSG = message.content.split(`${bot.prefix}shout ${rChannel.id}`).join(" ")
 if(!MSG)return = message.channel.send(`Tell me the message to announce.`)
 const _ = new MessageEmbed()
 .setTitle("Announcement")
